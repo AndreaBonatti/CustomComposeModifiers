91f920429d37f0fc0bb0bfa66e0a88b1e99e4476
Andrea Bonatti <bonand@ideabs.local>
2022-10-17 09:09:44 +0200

    Tutorial on how to create custom modifiers for Jetpack Compose

---
+++ app/src/main/java/com/example/customcomposemodifiers/MainActivity.kt
@@ -0,0 +1,65 @@
+package com.example.customcomposemodifiers
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.animation.core.animateFloat
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.rememberInfiniteTransition
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.composed
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.unit.dp
+import com.example.customcomposemodifiers.ui.theme.CustomComposeModifiersTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            CustomComposeModifiersTheme {
+                Box(
+                    modifier = Modifier.fillMaxSize(),
+                    contentAlignment = Alignment.Center
+                ) {
+                    Box(
+                        modifier = Modifier
+                            .size(100.dp)
+//                            .redBall()
+                            .rotating(3000)
+                            .background(Color.Red)
+                    )
+                }
+            }
+        }
+    }
+}
+
+// stateless modifier
+fun Modifier.redBall(): Modifier =
+    clip(CircleShape).background(Color.Red)
+
+// stateful modifier
+fun Modifier.rotating(duration: Int): Modifier = composed {
+    val transition = rememberInfiniteTransition()
+    val angleRatio by transition.animateFloat(
+        initialValue = 0f,
+        targetValue = 1f,
+        animationSpec = infiniteRepeatable(
+            animation = tween(durationMillis = duration)
+        )
+    )
+    graphicsLayer(
+        rotationZ = 360f * angleRatio
+    )
+}
+
